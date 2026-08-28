fbd6f3db103533c45a380e93f4883c09f5563d51
Prahlad Singh <prahladsingh7512@gmail.com>
2026-08-28 23:32:42 +0530

    Update working directory for brainvault-api

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -3,13 +3,14 @@ module.exports = {
     {
       name: 'brainvault-api',
       script: 'server.js',
-      cwd: '/home/prahlad/brainvault/backend',
+      cwd: '/var/www/projects/brainvault/backend',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
       env_production: {
         NODE_ENV: 'production',
+        PORT: 5005,
       },
     },
   ],
